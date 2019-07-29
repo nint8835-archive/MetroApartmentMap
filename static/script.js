b8bae9670fb971239ec3df9b6b55f6e335653c64
Riley Flynn <riley@rileyflynn.me>
2019-07-29 20:50:55 -0230

    Object destructuring time

--- static/script.js
+++ static/script.js
@@ -1,31 +1,31 @@
-function generatePopup(apartment) {
+function generatePopup({ attributes, title, description, url }) {
   const billsIncluded = [];
   ["Hydro", "Heat", "Water", "Cable TV", "Internet", "Landline"].forEach(
     bill => {
-      if (apartment.attributes[bill.toLowerCase().replace(" ", "")] == 1) {
+      if (attributes[bill.toLowerCase().replace(" ", "")] == 1) {
         billsIncluded.push(bill);
       }
     }
   );
   return `
-  <b>${apartment.title}</b><br>
-  ${apartment.description.replace("\\n", "<br>")}<br>
+  <b>${title}</b><br>
+  ${description}<br>
   <br>
-  <b>Bedrooms:</b> ${apartment.attributes.numberbedrooms}<br>
-  <b>Bathrooms:</b> ${apartment.attributes.numberbathrooms / 10}<br>
-  <b>Parking spots:</b> ${apartment.attributes.numberparkingspots}<br>
-  <b>Space:</b> ${apartment.attributes.areainfeet || 0} sqft<br>
-  <b>Furnished:</b> ${apartment.attributes.furnished ? "Yes" : "No"}<br>
-  <b>Yard:</b> ${apartment.attributes.yard ? "Yes" : "No"}<br>
-  <b>Balcony:</b> ${apartment.attributes.balcony ? "Yes" : "No"}<br>
-  <b>Pets allowed:</b> ${apartment.attributes.petsallowed ? "Yes" : "No"}<br>
+  <b>Bedrooms:</b> ${attributes.numberbedrooms}<br>
+  <b>Bathrooms:</b> ${attributes.numberbathrooms / 10}<br>
+  <b>Parking spots:</b> ${attributes.numberparkingspots}<br>
+  <b>Space:</b> ${attributes.areainfeet || 0} sqft<br>
+  <b>Furnished:</b> ${attributes.furnished ? "Yes" : "No"}<br>
+  <b>Yard:</b> ${attributes.yard ? "Yes" : "No"}<br>
+  <b>Balcony:</b> ${attributes.balcony ? "Yes" : "No"}<br>
+  <b>Pets allowed:</b> ${attributes.petsallowed ? "Yes" : "No"}<br>
   <b>Smoking allowed:</b> ${
-    ["No", "Yes", "Outdoors only"][apartment.attributes.smokingpermitted]
+    ["No", "Yes", "Outdoors only"][attributes.smokingpermitted]
   }<br>
   <b>Bills included:</b> ${billsIncluded.join(", ")}<br>
-  <b>Price:</b> \$${apartment.attributes.price}/month<br>
+  <b>Price:</b> \$${attributes.price}/month<br>
   <br>
-  <a href="${apartment.url}">${apartment.url}</a>
+  <a href="${url}">${url}</a>
   `;
 }
 
