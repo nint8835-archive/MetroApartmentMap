9bb27fb6bfcdfd67ce7460273580b6f3e9fa6a93
Riley Flynn <riley@rileyflynn.me>
2019-07-29 21:31:26 -0230

    Fixed server loading

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const apartments = require("./apartments.json");
 
-getApartments().then(apartments => {
-  const app = express();
-  app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: "static/" });
-  });
+const app = express();
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "static/" });
+});
 
-  app.get("/apartments", (req, res) => {
-    res.json(apartments);
-  });
+app.get("/apartments", (req, res) => {
+  res.json(apartments);
+});
 
-  app.use("/static", express.static("static"));
-  app.use("/node_modules", express.static("node_modules"));
+app.use("/static", express.static("static"));
+app.use("/node_modules", express.static("node_modules"));
 
-  app.listen(3000, () => {
-    console.log("Server listening on port 3000");
-  });
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
